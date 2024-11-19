2e4118128da1f8188f29a66c110d6c5ace2018b8
Danielvartatt <daniel1.kodehode@gmail.com>
2024-11-19 13:30:23 +0100

    oppgavene-ferdig-19.11

---
+++ oppgave-tekst.js
@@ -0,0 +1,46 @@
+/*
+console.log(cleanArray);
+Task 1 - Finn det laveste nummeret i et array
+Skriv en funksjon findMin som tar i mot en array med nummer og returnerer det laveste nummeret. Lag denne uten bruk av Math
+
+function findMin(arr){}
+findMin([1, 6, 2, 8, -1]) // skal returnere -1
+// Prøv med flere test cases!
+ 
+
+Task 2 - Tell vokaler i en string
+Skriv en funksjon countVowels som tar i mot en string og returnerer antall vokaler i stringen
+
+function countVowels(str){}
+countVowels("Hei på deg") // skal returnere 4
+// Prøv med flere test cases!
+ 
+
+Task 3 - Summer alle numrene i en range
+Skriv en funksjon sumRange som tar i mot to nummer, start og slutt og returnerer summen av alle numrene mellom start og slutt (inklusivt)
+
+function sumRange(start, end){}
+
+sumRange(1, 5) // skal returnere 15
+// Prøv med flere test cases!
+ 
+
+Task 4 -Tell hvor mange ganger en bokstav kommer opp i en string
+Skriv en funksjon countOccurences som tar i mot en string og en bokstav, og returner antall ganger som bokstaven er i stringen
+
+function countOccurences(str, char){}
+countOccurences("Hello World!", "l") // skal returnere 2
+countOccurences("Hello World!", "æ") // skal returnere 0
+// Prøv med flere test cases!
+ 
+
+//Task 5 - Fjern duplicates fra en array
+//Skriv en funksjon removeDuplicates som tar i mot en array, og returner et nytt array med duplikatene fjernet
+
+//Tips: Bruk et array unique, og sjekk !unique.includes(verdi)
+
+//function removeDuplicates(arr) {}
+removeDuplicates([1, 2, 2, 3, 4, 4, 5]); // skal returnere: [1, 2, 3, 4, 5]
+removeDuplicates(["Harald", "Håkon", "Harald", "Bjarne", "Grethe"]); // skal returnere: ["Harald", "Håkon", "Bjarne", "Grethe"]
+// Prøv med flere test cases!
+/*/
